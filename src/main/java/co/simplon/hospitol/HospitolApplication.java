382d65080d44234031a0310cf49a0ccc6fd9b3a8
YoanBor <yoanbor@icloud.com>
2024-03-26 14:11:31 +0100

    Mise en place de l'environnement Back

---
+++ src/main/java/co/simplon/hospitol/HospitolApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.hospitol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitolApplication.class, args);
+	}
+
+}
